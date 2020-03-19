d00f8fc7cc3b7392b79e9c6d76a9859753444b69
Eduard <stepanyan_e@inbox.ru>
2020-03-20 01:48:53 +0300

    adding a form for recording posts directly on the site

--- src/main/java/com/steppedua/blog/controllers/BlogController.java
+++ src/main/java/com/steppedua/blog/controllers/BlogController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -22,4 +24,19 @@ public class BlogController {
         return "blog-main";
     }
 
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model) {
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title,
+                              @RequestParam String anons,
+                              @RequestParam String full_text,
+                              Model model) {
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
 }
+
